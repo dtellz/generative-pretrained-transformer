18e4f0fb281bf29efd4a876c4fc0ce6da221002d
Diego <dtellez95@gmail.com>
2024-03-23 08:09:05 +0000

    add data chunk loader

--- main.py
+++ main.py
@@ -44,3 +44,39 @@ print(data.shape, data.dtype)
 print(data[:1000])
 # --------------------------
 
+# Set train and validation data:
+
+n = int(0.9*len(data)) # first 90% for training rest for validation
+train_data = data[:n]
+val_data = data[n:]
+
+block_size = 8 # maximum context length
+train_data[:block_size+1]
+
+
+torch.manual_seed(1337)
+batch_size = 4 # independet parallel sequences
+
+def get_batch(split):
+    data = train_data if split == 'train' else val_data
+    ix = torch.randint(len(data) - block_size, (batch_size,))
+    x = torch.stack([data[i:i+block_size] for i in ix])
+    y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+    return x, y
+
+xb, yb, = get_batch('train')
+print('--------------------------')
+print(f'[-] Input batch shape: {xb.shape} \n {xb}')
+print(f'[-] Target batch shape: {yb.shape} \n {yb}')
+print('--------------------------')
+
+for b in range(batch_size):
+    for t in range(block_size):
+        context = xb[b, :t+1]
+        target = yb[b, t]
+        print(f'[-] Context: {context.tolist()} -> Target: {target}')
+
+
+
+
+
